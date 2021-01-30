3614342c458913b8148eb2689b7f3f8c985cc41e
Federico Jimenez <fedeejimenez@gmail.com>
2021-01-30 19:09:56 +0100

    Add react routing fir index and show

---
+++ app/javascript/components/Airline/Airline.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airline = () => {
+  return <div>Airline show page</div>
+}
+
+export default Airline
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,16 +4,14 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 import App from '../components/App'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <Router>
+      <Route path="/" component={App}/>
+    </Router>,
     document.body.appendChild(document.createElement('div')),
   )
 })
