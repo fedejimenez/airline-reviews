a7a77f4c9fcaa18507560296cf2a6bd6f49a1316
Federico Jimenez <fedeejimenez@gmail.com>
2021-01-30 19:26:16 +0100

    Getting data from the api with axios

--- app/javascript/components/Airlines/Airlines.jsx
+++ app/javascript/components/Airlines/Airlines.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
-  return <div>Airlines index</div>
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    // get all airlines from api
+    // update airlines in state
+    axios.get('/api/v1/airlines')
+    .then( resp => {
+      setAirlines(resp.data.data)
+    } )
+    .catch( resp => console.log(resp) )
+  }, [airlines.length])
+
+  const list = airlines.map( item => {
+  return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  })
+
+  return (
+    <div className="home">
+      <div className="header">
+        <h1>Airline Reviews</h1>
+        <div className="subeader">Honest airline reviews</div>
+      </div>
+      <div className="grid">
+        <ul>{list}</ul>
+      </div>
+    </div>
+  )
 }
 
 export default Airlines
