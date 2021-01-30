68b6a94757a0f0ed639fc9b44147416f882baad2
Federico Jimenez <fedeejimenez@gmail.com>
2021-01-30 23:08:12 +0100

    Add component for review form

--- app/javascript/components/Airline/Airline.jsx
+++ app/javascript/components/Airline/Airline.jsx
@@ -1,6 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import Header from './Header'
+import ReviewForm from './ReviewForm'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+
+  &:last-child {
+    background: #000;
+  }
+`
+
+const Main = styled.div`
+  padding-left: 3rem;
+`
 
 const Airline = (props) => {
   const [airline, setAirline] = useState({})
@@ -20,20 +43,24 @@ const Airline = (props) => {
   }, [])
 
   return (
-    <div className="wrapper">
-      <div className="column">
-        { loaded &&
-          <Header 
-            attributes={airline.data.attributes}
-            reviews={airline.included}
-          />
-        }
-        <div className="reviews"></div>
-      </div>
-      <div className="column">
-        <div className="reviews-form"></div>
-      </div>
-    </div>
+    <Wrapper>
+      { loaded &&
+        <Fragment>
+          <Column>
+            <Main>
+                <Header 
+                  attributes={airline.data.attributes}
+                  reviews={airline.included}
+                />
+              <div className="reviews"></div>
+            </Main>
+          </Column>
+          <Column>
+            <ReviewForm/>
+          </Column>
+        </Fragment>
+      }
+    </Wrapper>
   )
 }
 
