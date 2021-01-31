029e0f2c9d3a57b551d46662a0919b8639052c6d
Federico Jimenez <fedeejimenez@gmail.com>
2021-01-31 17:43:55 +0100

    Fix issue with css files in production (#9)
    * Revert changes
    
    * Try to fix error loading css files in production
    
    * Try to fix error loading css files in production
    
    * Try to fix error loading css files in production

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Airlines from './Airlines/Airlines'
 import Airline from './Airline/Airline'
-import './App.scss'
+import '../stylesheets/App.css'
 
 const App = () => {
   return (
--- app/javascript/components/Rating/Rating.jsx
+++ app/javascript/components/Rating/Rating.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Rating.scss'
+import '../../stylesheets/Rating.css'
 
 const Rating = (props) => {
   const score = (props.score / 5) * 100
