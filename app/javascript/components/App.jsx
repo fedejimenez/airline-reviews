ee476220190b4639861a528345e920a2258e51f8
Federico Jimenez <fedeejimenez@gmail.com>
2021-01-31 15:22:24 +0100

    Fix sizing issues in review form

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Airlines from './Airlines/Airlines'
 import Airline from './Airline/Airline'
+import './App.css'
 
 const App = () => {
   return (
