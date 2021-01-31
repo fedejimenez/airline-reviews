f59379969eed55099d06878b7edc77aec489d187
Federico Jimenez <fedeejimenez@gmail.com>
2021-01-31 16:04:01 +0100

    Rename style files to scss

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Airlines from './Airlines/Airlines'
 import Airline from './Airline/Airline'
-import './App.css'
+import './App.scss'
 
 const App = () => {
   return (
