dd30defc6bfa00935b62c5197354afda0a4b888f
Federico Jimenez <fedeejimenez@gmail.com>
2021-02-06 13:26:20 +0100

    Add rubocop and github actions (#10)
    * Revert changes
    
    * Try to fix error loading css files in production
    
    * Try to fix error loading css files in production
    
    * Try to fix error loading css files in production
    
    * ADd rubocop and fix initial errors
    
    * Add github action

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+#:nodoc:
 class Review < ApplicationRecord
   belongs_to :airline
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,28 +12,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_153427) do
-
+ActiveRecord::Schema.define(version: 20_210_130_153_427) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "airlines", force: :cascade do |t|
-    t.string "name"
-    t.string "image_url"
-    t.string "slug"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'airlines', force: :cascade do |t|
+    t.string 'name'
+    t.string 'image_url'
+    t.string 'slug'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.integer "score"
-    t.bigint "airline_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["airline_id"], name: "index_reviews_on_airline_id"
+  create_table 'reviews', force: :cascade do |t|
+    t.string 'title'
+    t.string 'description'
+    t.integer 'score'
+    t.bigint 'airline_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['airline_id'], name: 'index_reviews_on_airline_id'
   end
 
-  add_foreign_key "reviews", "airlines"
+  add_foreign_key 'reviews', 'airlines'
 end
